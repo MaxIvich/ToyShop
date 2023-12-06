6895ccbe563d6eb2b3eddf16e49c40da5d34ee37
Maxivich <maxivlevich@gmail.com>
2023-12-06 21:29:29 +0300

    0.2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
 public class Main {
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    functions functions = new functions();
+    functions.createToy(1,"Car",25,50);
+    functions.createToy(2,"Doll",10,25);
+    functions.createToy(3,"Lego",3,1);
+    functions.createToy(4,"helicopter",7,8);
+
+
+        functions.getWinner();
+
+
     }
 }
+
